727cbd8039ec9b39cb380cc996c8c25c5974c678
IvanovMihai <cosmin1999@gmail.com>
2017-08-14 18:30:49 +0300

    Added fancy glow
    FPS , Glow based on distance , player , fucks given

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+var FPS = 60;
+
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function convertForTan(x){
+	return x;
 }
 
 // RequestAnimFrame: a browser API for getting smooth animations
@@ -10,7 +16,7 @@ window.requestAnimFrame = (function(){
 		window.oRequestAnimationFrame      || 
 		window.msRequestAnimationFrame     ||  
 		function( callback ){
-			return window.setTimeout(callback, 1000 / 60);
+			return window.setTimeout(callback, 1000 / FPS);
 		};
 })();
 
@@ -30,7 +36,7 @@ var canvas = document.querySelector('#game'),
     H = canvas.height,
     CL = 0, //camera left and camera top , used for camera following the player , yeah im dumb
     CT = 0,
-	speed = 10,
+	speed = 300 / FPS,
     init,
     mouse = {},
     startBtn = {},up,down,left,right,
@@ -42,8 +48,7 @@ canvas.addEventListener("mousedown", btnClick, true);
 canvas.addEventListener("keydown", fixedUpdate , true);
 canvas.addEventListener("keyup",stopmoving,true)
 
-ctx.shadowBlur = 8;
-ctx.shadowColor = "white";
+ctx.shadowBlur = 0;
 
 // Track the position of mouse cursor
 function trackPosition(e) {
@@ -69,13 +74,13 @@ ball = {
 };
 
 function ball2() {
-	this.x = W/2,
-	this.y = H/2, 
-	this.r = 10,
+	this.x = W/2;
+	this.y = H/2; 
+	this.r = 10;
 	this.fx = getRandomInt(0 , 100);
 	this.fy = getRandomInt(0 , 100);
-	this.c = "green",
-	this.speed =  10,
+	this.c = "green";
+	this.speed =  10;
 	// Function for drawing ball on canvas
 	this.draw = function() {
 		ctx.beginPath();
@@ -86,9 +91,60 @@ function ball2() {
 	}
 };
 
+function creature(type , c1 , ec){
+	this.x = W/2;
+	this.y = H/2;
+	this.fx = getRandomInt(0 , 400);
+	this.fy = getRandomInt(0 , 400);
+	this.r = 0;
+	this.ro = 0;
+	this.c1 = (c1 || "white");
+	this.ec = (ec || "red");
+	this.type = (type || "player");
+	this.w = (this.type === "player" ? 40 : 20 ); 
+	this.h = 1.1 * this.w;
+	this.draw = function(){
+		ctx.beginPath();
+		ctx.shadowColor = this.c1;
+		if (this.r){
+			//eyes
+			ctx.fillStyle = this.ec;
+			ctx.fillRect(this.x + this.w / 3  - this.ro * this.w * 2 / 15 , this.y - this.h * 6 / 10 , this.h / 7 , this.w / 5);
+			ctx.fillRect(this.x + this.w * 2 / 3 - this.ro * this.w * 2 / 15 , this.y - this.h * 6 / 10 , this.h / 7 , this.w / 5);
+		}
+		ctx.fillStyle = this.c1;
+		//body
+		ctx.fillRect(this.x, this.y, this.w ,this.h);
+		//head
+		ctx.fillRect(this.x + this.w / 4 , this.y - this.h / 2 , this.h / 2 , this.w / 2);
+		//legs
+		ctx.fillRect(this.x + this.w / 8, this.y + this.h * 1.1, this.w / 4 ,this.h * 1.1 / 4);
+		ctx.fillRect(this.x + this.w * 5/8 , this.y + this.h * 1.1, this.w / 4 ,this.h * 1.1 / 4);
+		//hands
+		ctx.fillRect(this.x - this.w / 4 , this.y, this.w / 4 ,this.h * 5 / 6);
+		ctx.fillRect(this.x + this.w , this.y, this.w / 4 ,this.h * 5 / 6);
+		if (!this.r){
+			//eyes
+			ctx.fillStyle = this.ec;
+			ctx.fillRect(this.x + this.w / 3 - this.ro * this.w * 2 / 15 , this.y - this.h * 6 / 10 , this.h / 7 , this.w / 5);
+			ctx.fillRect(this.x + this.w * 2 / 3 - this.ro * this.w * 2 / 15, this.y - this.h * 6 / 10 , this.h / 7 , this.w / 5);
+		}
+		
+		
+
+	}
+};
+
+function wall(){
+
+};
+
+
+var player = new creature();
+
 ballArr = [];
-for(var i = 0 ; i <= 1000 ; i++){
-	ballArr[i] = new ball2();
+for(var i = 0 ; i < 2 ; i++){
+	ballArr[i] = new creature("monster", "red" , "white");
 	ballArr[i].x = ballArr[i].fx;
 	ballArr[i].y = ballArr[i].fy;
 }
@@ -129,22 +185,26 @@ function fixedUpdate(e){
     if (code == 87 && !up){
 		up = setInterval(function(){
 			CT -= speed;
-		}, 1000/45);
+		}, 1000/FPS);
+		player.r = 1;
 	}
 	else if(code == 83 && !down){
 		down = setInterval(function(){
 			CT += speed;
-		}, 1000/45);
+		}, 1000/FPS);
+		player.r = 0;
 	}
 	else if(code == 65 && !left){
 		left = setInterval(function(){
 			CL -= speed;
-		}, 1000/45);
+		}, 1000/FPS);
+		player.ro = 1;
 	}
 	else if(code == 68 && !right){
 		right = setInterval(function(){
 			CL += speed;
-		}, 1000/45);
+		}, 1000/FPS);
+		player.ro = 0;
 	}
 }
 
@@ -168,11 +228,24 @@ function stopmoving(e){
 	}
 }
 
+var maxD = Infinity; 
+
 // A blind man walks into a bar... and a table and a chair
 function update(){
-	for(var i = 0 ; i <= 100 ; i++){
+	for(var i = 0 ; i < ballArr.length ; i++){
+		var maxD = Infinity;
 		ballArr[i].x = ballArr[i].fx -  CL;
 		ballArr[i].y = ballArr[i].fy - CT;
+		if(maxD > (ballArr[i].x - W/2) * (ballArr[i].x - W/2) + (ballArr[i].y - H/2) * (ballArr[i].y - H/2)){
+			maxD = (ballArr[i].x - W/2) * (ballArr[i].x - W/2) + (ballArr[i].y - H/2) * (ballArr[i].y - H/2);
+			maxD += 1;
+			if(maxD > 300000){
+				ctx.shadowBlur = 0;
+			}
+			else {
+				ctx.shadowBlur = Math.floor(100 - maxD / 3000);
+			}
+		}
 	}
 }
 
@@ -180,10 +253,11 @@ function update(){
 function draw() {
 	start();
 	update();
-	ball.draw();
-	for(var i = 0 ; i <= 100 ; i++){
-		ballArr[i].draw();
+	player.draw();
+	for(var i = 0 ; i < ballArr.length ; i++){
+		ballArr[i].draw();	
 	}
+	
 }
 
 function gameOver() {
