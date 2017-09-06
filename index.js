7c0eed649eb2a1466ca32e481d4185cc87eaef77
IvanovMihai <cosmin1999@gmail.com>
2017-09-06 19:08:07 +0300

    getting ready to ship <3
    it still stucks

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var FPS = 60;
-var imgURL = ['img/fata.png' ,'img/mersulpinguinilor.png','img/spate.png', 'img/reversed-min.png'];
+var imgURL = ['img/fata.png', 'img/mersulpinguinilor.png', 'img/spate.png', 'img/reversed-min.png'];
 var images = [];
-var dict = ['fear' , 'happiness' , 'shame' , 'remorseless', 'anguish' , 'delight' , 'sadness' , 'cheer' , 'heartache' , 'contentment' ,'suffering' , 'comfort' , 'collapse' , 'wonder'];
+var dict = ['fear', 'happiness', 'shame', 'remorseless', 'anguish', 'delight', 'sadness', 'cheer', 'heartache', 'contentment', 'suffering', 'comfort', 'collapse', 'wonder'];
 var imgNr = imgURL.length;
 // Initialize some stuff
 var canvas = document.querySelector('#game'),
@@ -18,14 +18,18 @@ var canvas = document.querySelector('#game'),
 	maxParticles = 10,
 	startBtn = {}, up, down, left, right,
 	restartBtn = {},
-	mainPlay = 0,
+	mainPlay = 1,
 	wordIndex = 0,
-	wordPlay = new Word(400 , 200 , [255, 255 , 255] , 1 , [dict[1], 1] , 40 ),
-	wordTyped = new Word(400 , 250 , [0 , 255 , 0] , 1 , ['',1] , 40),
+	wordPlay = new Word(400, 200, [255, 255, 255], 1, ['', 1], 40),
+	wordTyped = new Word(400, 250, [0, 255, 0], 1, ['', 1], 40),
+	text = new Word(W / 2, 75, [0, 255, 0], 1, ['', 1], 40),
+	score = 0,
 	timeout = 0,
 	life = 3,
 	currentTime,
-	lastTime = 0;
+	lastTime = 0,
+	goalIndex = 0
+	howMany = 0;
 
 
 ctx.mozImageSmoothingEnabled = false;
@@ -33,8 +37,9 @@ ctx.webkitImageSmoothingEnabled = false;
 ctx.msImageSmoothingEnabled = false;
 ctx.imageSmoothingEnabled = false;
 
-function magicFunc(x){ //I obtained it using linear regression, used it for getting the number of words to be displayed
-	return -0.01127*x + 55.38;
+function magicFunc(X) { //I obtained it using linear regression, used it for getting the number of words to be displayed
+	return -0.0646*X + 55.84
+
 }
 
 function getRandomInt(min, max) {
@@ -45,28 +50,34 @@ function isInArray(value, array) {
 	return array.indexOf(value) > -1;
 }
 
-function circleCoords(x){
-	var random = getRandomInt(0 , 2 * Math.PI * 100) / 100;
-	return [x * 3 * Math.cos(random) + W/2 + getRandomInt(- 20 , 20) , x * 3 *  Math.sin(random) + H/2 + getRandomInt(-20 , 20) ];
+function circleCoords(x) {
+	var random = getRandomInt(0, 2 * Math.PI * 100) / 100;
+	return [x * 3 * Math.cos(random) + W / 2 + getRandomInt(- 20, 20), x * 3 * Math.sin(random) + H / 2 + getRandomInt(-20, 20)];
+}
+
+function findDistance(x , y){
+	var a = x - W/2;
+	var b = y - H/2;
+	return Math.sqrt(a*a + b*b);
 }
 
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+	var currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-  return array;
+	return array;
 }
 
 
@@ -92,18 +103,17 @@ function makeWall(x1, y1, x2, y2) {
 	//WHACHAAAUT
 	//maze.push(new Wall((x1===x2) * x1 , (y2 === y1) * y1 , scale * Math.abs((x1 - x2) || (y1 - y2)) , FUCKME ));
 	//I will keep that line to see the reason I cry
-	if(x1 === x2)
-	{
-		maze.push(new Wall(x1 , Math.min(y2,y1) , 10 , Math.abs(y2 - y1) ));
+	if (x1 === x2) {
+		maze.push(new Wall(x1, Math.min(y2, y1), 10, Math.abs(y2 - y1)));
 	}
-	else if(y2 === y1){
-		maze.push(new Wall(Math.min(x1,x2) , y1 + 10 , Math.abs(x1 - x2) , 10));
+	else if (y2 === y1) {
+		maze.push(new Wall(Math.min(x1, x2), y1 + 10, Math.abs(x1 - x2), 10));
 	}
 }
 
 function generator(x, y) {
 	matrix[x][y] = 1;
-	var arr = [1,2,3,4];
+	var arr = [1, 2, 3, 4];
 	arr = shuffle(arr);
 	for (i = 0; i < 4; i++) {
 		if (arr[i] === 1 && matrix[x - 1] && matrix[x - 1][y] === 0) {
@@ -114,17 +124,17 @@ function generator(x, y) {
 		if (arr[i] === 2 && matrix[x + 1] && matrix[x + 1][y] === 0) {
 			makeWall((x + 1) * scale, y * scale, x * scale, y * scale);
 			return generator(x + 1, y);
-			
+
 		}
 
-		if (arr[i] === 3  && matrix[x][y - 1] === 0) {
+		if (arr[i] === 3 && matrix[x][y - 1] === 0) {
 			makeWall(x * scale, (y - 1) * scale, x * scale, y * scale);
 			return generator(x, y - 1);
-			
+
 		}
 
-		if (arr[i] === 4  && matrix[x][y + 1] === 0) {
-			makeWall(x * scale, (y + 1) * scale, x  * scale, y * scale);
+		if (arr[i] === 4 && matrix[x][y + 1] === 0) {
+			makeWall(x * scale, (y + 1) * scale, x * scale, y * scale);
 			return generator(x, y + 1);
 		}
 	}
@@ -181,8 +191,8 @@ function trackPosition(e) {
 
 //TODO
 function Creature(type, c1, ec, sc) {
-	this.x = W / 2 - scale/2;
-	this.y = H / 2 - scale/2;
+	this.x = W / 2 - scale / 2;
+	this.y = H / 2 - scale / 2;
 	this.fx = getRandomInt(0, 400);
 	this.fy = getRandomInt(0, 400);
 	this.r = 0;
@@ -195,21 +205,17 @@ function Creature(type, c1, ec, sc) {
 	this.h = 1.1 * this.w;
 	this.draw = function () {
 		ctx.shadowBlur = 100;
-		if(this.r === 0)
-		{
-			ctx.drawImage(images[0],this.x,this.y , scale, scale);
+		if (this.r === 0) {
+			ctx.drawImage(images[0], this.x, this.y, scale, scale);
 		}
-		else if(this.r === 1)
-		{
-			ctx.drawImage(images[2], this.x , this.y , scale, scale);
+		else if (this.r === 1) {
+			ctx.drawImage(images[2], this.x, this.y, scale, scale);
 		}
-		else if(this.r === 3)
-		{
-			ctx.drawImage(images[1] , this.x , this.y , scale, scale);
+		else if (this.r === 3) {
+			ctx.drawImage(images[1], this.x, this.y, scale, scale);
 		}
-		else if(this.r === 2)
-		{
-			ctx.drawImage(images[3] , this.x , this.y, scale, scale);
+		else if (this.r === 2) {
+			ctx.drawImage(images[3], this.x, this.y, scale, scale);
 		}
 	}
 };
@@ -232,30 +238,30 @@ function Wall(x, y, w, h) {
 };
 
 //c = [R , G , B] , 0 <= alpha <= 1
-function Word(x , y , c , alpha , word , size){
+function Word(x, y, c, alpha, word, size) {
 	this.x = x;
 	this.y = y;
 	this.w = word;
-	if(c.length){
+	if (c.length) {
 		this.c = c;
 	}
 	else {
-		if(dict[word[1]] === dict[word[1] + 1]){
-			this.c = [0 , 255 , 0];
+		if (dict[word[1]] === dict[word[1] + 1]) {
+			this.c = [0, 255, 0];
 		}
 		else {
-			this.c = [255 , 0 , 0];
+			this.c = [255, 0, 0];
 		}
-	}			
+	}
 	this.alpha = alpha;
-	this.size = size || getRandomInt(15 , 30);
-	this.draw = function(){
+	this.size = size || getRandomInt(15, 30);
+	this.draw = function () {
 		ctx.beginPath();
 		ctx.shadowBlur = 0;
 		ctx.font = this.size.toString() + "px Impact, Charcoal, sans-serif";
 		ctx.textAlign = "center";
-		ctx.fillStyle = "rgba(" + this.c[0] +", " + this.c[1] + ", "+ this.c[2] + ","  + this.alpha + ")";
-		ctx.fillText(this.w[0] , this.x , this.y);
+		ctx.fillStyle = "rgba(" + this.c[0] + ", " + this.c[1] + ", " + this.c[2] + "," + this.alpha + ")";
+		ctx.fillText(this.w[0], this.x, this.y);
 	}
 }
 
@@ -263,15 +269,14 @@ function Word(x , y , c , alpha , word , size){
 var player = new Creature();
 
 wordArr = [];
-for (var i = 0; i < maxWords; i++) {
+/*for (var i = 0; i < maxWords; i++) {
 	wordArr[i] = new Word(0,0, [] , 1 , [dict[2 * getRandomInt(0, dict.length/2 - 1)] , 2 * getRandomInt(0, dict.length/2 - 1)]);
 	var XoY = circleCoords(80);
 	wordArr[i].x = XoY[0];
 	wordArr[i].y = XoY[1];
-}
+}*/
 
 function updateWords(){
-	var count = maxWords - wordArr.length;
 	//TODO
 	for(var i = 0 ; i < wordArr.length; i++){
 		if(wordArr[i]){
@@ -282,12 +287,16 @@ function updateWords(){
 			}
 		}
 	}
-	for(var i = 0 ; i < count ; i++){
-		var XoY = circleCoords(80);
-		wordArr.push(new Word(XoY[0] ,XoY[1] , [] , 1 , [dict[2 * getRandomInt(0, dict.length/2 - 1)] , 2 * getRandomInt(0, dict.length/2 - 1)]));
-	}
-
 }
+
+function pushWord(word){
+	if(maxWords > wordArr.length){
+		var XoY = circleCoords(80);
+		wordArr.push(new Word(XoY[0] , XoY[1] , [] , 1 , [word , 1] ));
+	}
+}
+
+
 
 // Start Button object
 startBtn = {
@@ -312,18 +321,21 @@ startBtn = {
 	}
 };
 
-function Goal (x , y){
+function Goal(x, y, yee) {
 	this.x = x;
 	this.fx = x;
 	this.y = y;
 	this.fy = y;
-	this.draw = function(){
+	this.yee = yee;
+	this.c = (yee ? "#00FF00" : "red");
+	this.draw = function () {
 		ctx.shadowBlur = 0;
 		ctx.beginPath();
-		ctx.fillStyle = "red";
-		ctx.fillRect(this.x, this.y, scale / 2 , scale / 2);
+		ctx.fillStyle = this.c;
+		ctx.fillRect(this.x, this.y, scale / 2, scale / 2);
 	}
 }
+
 
 // Function for (re)start
 function start() {
@@ -336,21 +348,21 @@ function start() {
 function isClear(where) {
 	if (where === 'up') {
 		for (var i = 0; i < maze.length; i++) {
-			if ((  (H/2 - 5 * scale/12 - maze[i].y ) <= speed * 1.5) && (maze[i].y < H/2) && (maze[i].x <= W / 2) && (maze[i].x + maze[i].w >= W / 2)) {
+			if (((H / 2 - 5 * scale / 12 - maze[i].y) <= speed * 1.5) && (maze[i].y < H / 2) && (maze[i].x <= W / 2) && (maze[i].x + maze[i].w >= W / 2)) {
 				return 0;
 			}
 		}
 	}
 	else if (where === 'down') {
 		for (var i = 0; i < maze.length; i++) {
-			if ((  (maze[i].y - 0.3* scale - H/2 ) <= speed * 1.5) && (maze[i].y > H/2) && (maze[i].x <= W / 2) && (maze[i].x + maze[i].w >= W / 2)) {
+			if (((maze[i].y - 0.3 * scale - H / 2) <= speed * 1.5) && (maze[i].y > H / 2) && (maze[i].x <= W / 2) && (maze[i].x + maze[i].w >= W / 2)) {
 				return 0;
 			}
 		}
 	}
 	else if (where === 'left') {
 		for (var i = 0; i < maze.length; i++) {
-			if (((W/2 - scale * 0.1 - maze[i].x - maze[i].w) <= speed * 1.5 ) && (maze[i].x < W/2) && (maze[i].y + maze[i].h) >= (H / 2 -  scale/4 ) && maze[i].y <= (H / 2 +  0.20 * scale )) {
+			if (((W / 2 - scale * 0.1 - maze[i].x - maze[i].w) <= speed * 1.5) && (maze[i].x < W / 2) && (maze[i].y + maze[i].h) >= (H / 2 - scale / 4) && maze[i].y <= (H / 2 + 0.20 * scale)) {
 				return 0;
 			}
 		}
@@ -360,7 +372,7 @@ function isClear(where) {
 			/*console.log("start r");
 			console.log((maze[i].x - W/2 - scale/10)  <= speed * 1.5 );
 			console.log((maze[i].x > W/2)); */
-			if (((maze[i].x - W/2 - scale/10)  <= speed * 1.5 ) && (maze[i].x > W/2) && (maze[i].y + maze[i].h) >= (H / 2 -  scale/4 ) && maze[i].y <= (H / 2 +  0.20 * scale )) {
+			if (((maze[i].x - W / 2 - scale / 10) <= speed * 1.5) && (maze[i].x > W / 2) && (maze[i].y + maze[i].h) >= (H / 2 - scale / 4) && maze[i].y <= (H / 2 + 0.20 * scale)) {
 				return 0;
 			}
 		}
@@ -370,7 +382,7 @@ function isClear(where) {
 }
 
 function fixedUpdate(e) {
-	if(mainPlay){
+	if (mainPlay) {
 		var code = e.keyCode;
 		if (code == 87 && !up) {
 			up = setInterval(function () {
@@ -407,25 +419,28 @@ function fixedUpdate(e) {
 	}
 	else {
 		var code = e.charCode || e.keyCode || e.which;
-		if((code === 8 || code === 46) && wordTyped.w[0].length > 0){
-		wordTyped.w[0] = wordTyped.w[0].slice(0, -1);
+		if ((code === 8 || code === 46) && wordTyped.w[0].length > 0) {
+			wordTyped.w[0] = wordTyped.w[0].slice(0, -1);
 		}
-		else if(code === 13){
-			if(wordTyped.w[0] === wordPlay.w[0]){
-				dict[2 * wordIndex] = dict[2 * wordIndex + 1];
-				wordPlay.w = [dict[2 * ++wordIndex + 1] , 2 * wordIndex + 1];
+		else if (code === 13) {
+			if (wordTyped.w[0] === wordPlay.w[0]) {
 				lastTime = currentTime;
+				goals[goalIndex].yee = dict[goalIndex * 2 + 1];
+				goals[goalIndex].c = "#00FF00";
+				text.w[0] = dict[i * 2 + 1];
+				score++;
+				mainPlay = 1;
 			}
 			else {
-				wordPlay.w = [dict[2 * ++wordIndex + 1] , 2 * wordIndex + 1];
-				lastTime = currentTime - 2000;
+				mainPlay = 1;
+				goals[goalIndex].fx = getRandomInt(1, 24 * scale) - scale / 4;
 			}
-			wordTyped.w = ['',1];
+			wordTyped.w = ['', 1];
 		}
-		else{
+		else {
 			var keychar = String.fromCharCode(code);
 			var char = keychar.toLowerCase();
-			if(char.length === 1 && char.match(/[a-z]/i)){
+			if (char.length === 1 && char.match(/[a-z]/i)) {
 				wordTyped.w[0] += char;
 			}
 		}
@@ -434,27 +449,22 @@ function fixedUpdate(e) {
 }
 
 function stopmoving(e) {
-	if(mainPlay){
-		var code = e.keyCode;
-		if (code == 87 && up) {
-			clearInterval(up);
-			up = 0;
-		}
-		else if (code == 83 && down) {
-			clearInterval(down);
-			down = 0;
-		}
-		else if (code == 65 && left) {
-			clearInterval(left);
-			left = 0;
-		}
-		else if (code == 68 && right) {
-			clearInterval(right);
-			right = 0;
-		}
+	var code = e.keyCode;
+	if (code == 87 && up) {
+		clearInterval(up);
+		up = 0;
 	}
-	else {
-
+	else if (code == 83 && down) {
+		clearInterval(down);
+		down = 0;
+	}
+	else if (code == 65 && left) {
+		clearInterval(left);
+		left = 0;
+	}
+	else if (code == 68 && right) {
+		clearInterval(right);
+		right = 0;
 	}
 }
 
@@ -464,32 +474,71 @@ var maxD = Infinity;
 
 // A blind man walks into a bar... and a table and a chair
 function update() {
-	if(mainPlay){
-		var a = goal.x - W/2;
-		var b = goal.y - H/2
-		maxWords = Math.max(1 , Math.min(50 , Math.floor( magicFunc(Math.sqrt(a * a + b * b))  )));
+	if (mainPlay) {
+		target = {
+			i: 0,
+			d: Infinity
+		}
+		//maxWords = Math.max(1 , Math.min(50 , Math.floor( magicFunc(Math.sqrt(a * a + b * b))  )));
 		for (var i = 0; i < maze.length; i++) {
 			maze[i].x = maze[i].fx - CL;
 			maze[i].y = maze[i].fy - CT;
 		}
-		goal.x = goal.fx - CL;
-		goal.y = goal.fy - CT;
+		for (var i = 0; i < goals.length; i++) {
+			goals[i].x = goals[i].fx - CL;
+			goals[i].y = goals[i].fy - CT;
+			if(!goals[i].yee){
+				if(findDistance(goals[i].x , goals[i].y ) < target.d){
+					target.d = findDistance(goals[i].x , goals[i].y );
+					target.i = i;
+				}
+			}
+			if (goals[i].x - scale * 0.3 <= W / 2 && goals[i].x + 0.7 * scale >= W / 2) {
+				if (goals[i].y - scale / 4 <= H / 2 && goals[i].y + 0.7 * scale >= H / 2) {
+					if (goals[i].yee) {
+						text.w[0] = dict[i * 2 + 1];
+						text.draw();
+					}
+					else {
+						mainPlay = 0;
+						wordPlay.w[0] = dict[i * 2 + 1]; 
+						clearInterval(up);
+						up = 0;
+						clearInterval(left);
+						left = 0;
+						clearInterval(down);
+						down = 0;
+						clearInterval(right);
+						right = 0;
+						goalIndex = i;
+					}
+				}
+				else {
+					text.w[0] = '';
+				}
+			}
+			else {
+				text.w[0] = '';
+			}
+		}
+		maxWords = magicFunc(target.d);
 		updateWords();
+		pushWord(dict[2 * target.i]);
 	}
 	else {
-		if(!lastTime) {
-        	lastTime = currentTime;
-    	}
-		if(currentTime - lastTime >= 3 * 1000){
+		if (!lastTime) {
+			lastTime = currentTime;
+		}
+		if (currentTime - lastTime >= 3 * 1000) {
 			lastTime = currentTime;
 			mainPlay = 1;
-			wordArr = [];
+			/*wordArr = [];
 			for (var i = 0; i < maxWords; i++) {
 				wordArr[i] = new Word(0,0, [] , 1 , [dict[2 * getRandomInt(0, dict.length/2 - 1)] , 2 * getRandomInt(0, dict.length/2 - 1)]);
 				var XoY = circleCoords(80);
 				wordArr[i].x = XoY[0];
 				wordArr[i].y = XoY[1];
-			}
+			}*/
 		}
 	}
 }
@@ -498,22 +547,29 @@ function update() {
 function draw() {
 	start();
 	update();
-	if(mainPlay){
+	if (mainPlay) {
+		for (var i = 0; i < goals.length; i++) {
+			goals[i].draw();
+		}
 		player.draw();
-		goal.draw();
 		for (var i = 0; i < maze.length; i++) {
 			maze[i].draw();
 		}
 		for (var i = 0; i < wordArr.length; i++) {
 			wordArr[i].draw();
 		}
+		ctx.beginPath();
+		ctx.shadowBlur = 0 ;
+		ctx.font = "40px Impact, Charcoal, sans-serif";
+		ctx.textAlign = "center";
+		ctx.fillStyle = "#00FF00";
+		ctx.fillText(score + "/" + dict.length/2 , 50 , 75);
 	}
 	else {
 		ctx.shadowBlur = 0;
 		ctx.beginPath();
 		wordPlay.draw();
 		wordTyped.draw();
-		
 	}
 
 
@@ -530,18 +586,18 @@ function animloop(now) {
 	draw();
 }
 
-var goal = new Goal (100 , 100);
-
+var goals = [];
 function startScreen() {
-	mazeGenerator(50, 50);
-	for(i = 0 ; i < matrix.length ; i++){
-		for(j = 0 ; j < matrix.length; j++)
-		{
-			if(matrix[i][j] === 0)
-			{
-				generator(i,j);
+	mazeGenerator(25, 25);
+	for (i = 0; i < matrix.length; i++) {
+		for (j = 0; j < matrix.length; j++) {
+			if (matrix[i][j] === 0) {
+				generator(i, j);
 			}
 		}
+	}
+	for (i = 0; i < 7; i++) {
+		goals[i] = new Goal(getRandomInt(1, 24 * scale) - scale / 4, getRandomInt(1, 24 * scale) - scale / 4);
 	}
 	draw();
 	startBtn.draw();
@@ -559,7 +615,7 @@ function btnClick(e) {
 	if (mx >= startBtn.x && mx <= startBtn.x + startBtn.w && my >= startBtn.y && my <= startBtn.y + startBtn.h) {
 		animloop();
 		//TODO
-		setTimeout(function(){ console.log("hello"); }, 1000);
+		setTimeout(function () { console.log("hello"); }, 1000);
 		// Delete the start button after clicking it
 		startBtn = {};
 	}
@@ -567,27 +623,27 @@ function btnClick(e) {
 
 }
 
-for(var i = 0; i < imgURL.length; i++) {
+for (var i = 0; i < imgURL.length; i++) {
 
-    /// create a new image element
-    var img = new Image();
+	/// create a new image element
+	var img = new Image();
 
-    /// element is valid so we can push that to stack
-    images.push(img);
+	/// element is valid so we can push that to stack
+	images.push(img);
 
-    /// set handler and url
-    img.onload = onloadHandler;
-    img.src = imgURL[i];
+	/// set handler and url
+	img.onload = onloadHandler;
+	img.src = imgURL[i];
 
-    /// if image is cached IE (surprise!) may not trigger onload
-    if (img.complete) onloadHandler().bind(img);
+	/// if image is cached IE (surprise!) may not trigger onload
+	if (img.complete) onloadHandler().bind(img);
 }
 
 
 function onloadHandler() {
-    /// optionally: "this" contains current image just loaded
-    imgNr--;
-    if (imgNr === 0) startScreen();
+	/// optionally: "this" contains current image just loaded
+	imgNr--;
+	if (imgNr === 0) startScreen();
 }
 
 
